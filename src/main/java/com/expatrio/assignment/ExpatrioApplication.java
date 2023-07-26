8a5ef00011e13bdfb3f257c54e6e4ec0263ba63e
austinlewis <austinlewis1996@gmail.com>
2023-07-27 00:50:30 +0200

    misc: main application

---
+++ src/main/java/com/expatrio/assignment/ExpatrioApplication.java
@@ -0,0 +1,13 @@
+package com.expatrio.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpatrioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpatrioApplication.class, args);
+    }
+
+}
